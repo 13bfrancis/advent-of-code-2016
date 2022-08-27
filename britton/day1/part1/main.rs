099986a195ac2003a616c25203e530a82472b3ad
bdriggs <65035792+bdriggs@users.noreply.github.com>
2022-08-27 08:13:17 -0700

    Update main.rs

--- britton/day1/part1/main.rs
+++ britton/day1/part1/main.rs
@@ -1,52 +1,53 @@
 use std::fs;
 
 fn main() {
-    let input = read_file_string("input.txt").unwrap();
-    let moves: Vec<String> = input.split(", ").map(str::to_string).collect();
+    let input_data = read_file_string("input.txt").unwrap();
+    let moves: Vec<String> = input_data.split(", ").map(str::to_string).collect();
     let mut current_coordinates: [i32; 2] = [0, 0];
     let mut axis: i32 = 1;
     let mut direction: i32 = 1;
 
-    for next_move in moves {
+    moves.into_iter().for_each(|next_move| {
         let input_string = &String::from_iter(next_move.chars());
         let next_turn = input_string.chars().next().unwrap().to_string();
         let num_blocks = crop_letters(input_string, 1).parse::<i32>().unwrap();
-        let is_pos_x = axis == 0 && direction == 1;
-        let is_neg_x = axis == 0 && direction == -1;
-        let is_pos_y = axis == 1 && direction == 1;
-        let is_neg_y = axis == 1 && direction == -1;
+        let pos_x = axis == 0 && direction == 1;
+        let neg_x = axis == 0 && direction == -1;
+        let pos_y = axis == 1 && direction == 1;
+        let neg_y = axis == 1 && direction == -1;
+        let next_turn_right = next_turn == "R";
 
-        if is_pos_x {
+        if pos_x {
             axis = 1;
-            if next_turn == "R" {
+            if next_turn_right {
                 direction = -1;
             } else {
                 direction = 1;
             }
-        } else if is_neg_x {
+        } else if neg_x {
             axis = 1;
-            if next_turn == "R" {
+            if next_turn_right {
                 direction = 1;
             } else {
                 direction = -1;
             }
-        } else if is_pos_y {
+        } else if pos_y {
             axis = 0;
-            if next_turn == "R" {
+            if next_turn_right {
                 direction = 1;
             } else {
                 direction = -1;
             }
-        } else if is_neg_y {
+        } else if neg_y {
             axis = 0;
-            if next_turn == "R" {
+            if next_turn_right {
                 direction = -1;
             } else {
                 direction = 1;
             }
         }
         current_coordinates = update_coordinate((axis, num_blocks, direction, current_coordinates));
-    }
+    });
     println!("{:?}", current_coordinates);
     println!("{:?}", current_coordinates[0].abs() + current_coordinates[1].abs());
 }

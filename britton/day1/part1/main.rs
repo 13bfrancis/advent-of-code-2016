f3f824f22739b9cbd8e38031bbcbce297dbd3b57
bdriggs <65035792+bdriggs@users.noreply.github.com>
2022-08-27 13:22:38 -0700

    New

--- britton/day1/part1/main.rs
+++ britton/day1/part1/main.rs
@@ -3,9 +3,9 @@ use std::{collections::HashMap, fs};
 fn main() {
     let input_data: String = read_file_string("input.txt").unwrap();
     let moves: Vec<String> = input_data.split(", ").map(str::to_string).collect();
-    let mut current_coordinates: [i32; 2] = [0, 0];
+    let mut location: [i32; 2] = [0, 0];
     let mut axis: i32 = 1;
-    let mut direction: i32 = 1;
+    let mut directionality: i32 = 1;
     let axis_map = HashMap::from([(0, 1), (1, 0)]);
 
     moves.into_iter().for_each(|next_move: String| {
@@ -17,47 +17,38 @@ fn main() {
             .to_string()
             .to_lowercase();
         let num_blocks: i32 = crop_letters(next_input, 1).parse::<i32>().unwrap();
-        let pos_x: bool = axis == 0 && direction == 1;
-        let neg_x: bool = axis == 0 && direction == -1;
-        let pos_y: bool = axis == 1 && direction == 1;
-        let neg_y: bool = axis == 1 && direction == -1;
-        let next_turn_right: bool = next_turn == "r";
+        let east: bool = axis == 0 && directionality == 1;
+        let south: bool = axis == 1 && directionality == -1;
         axis = axis_map.get(&axis).unwrap().clone();
 
-        match pos_x || neg_y {
+        match east || south {
             true => {
-                direction = 1;
-                if next_turn_right {
-                    direction = -1;
+                directionality = 1;
+                if next_turn == "r" {
+                    directionality = -1;
                 }
             }
-            _ => {}
-        };
-
-        match neg_x || pos_y {
-            true => {
-                direction = -1;
-                if next_turn_right {
-                    direction = 1;
+            _ => {
+                directionality = -1;
+                if next_turn == "r" {
+                    directionality = 1;
                 }
             }
-            _ => {}
         };
-
-        current_coordinates = update_coordinate((axis, num_blocks, direction, current_coordinates));
+        location = update_location((axis, num_blocks, directionality, location));
     });
-    println!("{:?}", current_coordinates);
+    println!("{:?}", location);
     println!(
         "{:?}",
-        current_coordinates[0].abs() + current_coordinates[1].abs()
+        location[0].abs() + location[1].abs()
     );
 }
 
-fn update_coordinate(
-    (axis, input_mult, direction, mut current_coordinates): (i32, i32, i32, [i32; 2]),
+fn update_location(
+    (axis, input_mult, directionality, mut location): (i32, i32, i32, [i32; 2]),
 ) -> [i32; 2] {
-    current_coordinates[axis as usize] += input_mult * direction;
-    return current_coordinates;
+    location[axis as usize] += input_mult * directionality;
+    return location;
 }
 
 fn crop_letters(s: &str, pos: usize) -> &str {

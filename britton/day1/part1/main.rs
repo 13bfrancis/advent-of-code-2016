734acb8ae0811904e8c299ba8ee625dda464061a
bdriggs <65035792+bdriggs@users.noreply.github.com>
2022-08-27 07:35:56 -0700

    Update main.rs

--- britton/day1/part1/main.rs
+++ britton/day1/part1/main.rs
@@ -1,62 +1,61 @@
+use std::fs;
+
 fn main() {
-    let dirs: String = String::from("R4, R3, R5, L3, L5, R2, L2, R5, L2, R5, R5, R5, R1, R3, L2, L2, L1, R5, L3, R1, L2, R1, L3, L5, L1, R3, L4, R2, R4, L3, L1, R4, L4, R3, L5, L3, R188, R4, L1, R48, L5, R4, R71, R3, L2, R188, L3, R2, L3, R3, L5, L1, R1, L2, L4, L2, R5, L3, R3, R3, R4, L3, L4, R5, L4, L4, R3, R4, L4, R1, L3, L1, L1, R4, R1, L4, R1, L1, L3, R2, L2, R2, L1, R5, R3, R4, L5, R2, R5, L5, R1, R2, L1, L3, R3, R1, R3, L4, R4, L4, L1, R1, L2, L2, L4, R1, L3, R4, L2, R3, L1, L5, R4, R5, R2, R5, R1, R5, R1, R3, L3, L2, L2, L5, R2, L2, R5, R5, L2, R3, L5, R5, L2, R4, R2, L1, R3, L5, R3, R2, R5, L1, R3, L2, R2, R1");
-    let dir_vect: Vec<String> = dirs.split(", ").map(str::to_string).collect();
-    let mut current_loc: [i32; 2] = [0, 0];
-    let mut coord_to_mod: i32 = 1;
-    let mut current_mult: i32 = 1;
+    let input = read_file_string("input.txt").unwrap();
+    let dir_vect: Vec<String> = input.split(", ").map(str::to_string).collect();
+    let mut current_coordinates: [i32; 2] = [0, 0];
+    let mut axis: i32 = 1;
+    let mut direction: i32 = 1;
 
     for dir in dir_vect {
-        let is_negative = current_mult == -1;
-        let is_positive = current_mult == 1;
-        let is_x = coord_to_mod == 0;
-        let is_y = coord_to_mod == 1;
-        let direction = dir.chars().nth(0).unwrap().to_string();
-        let input_text = dir
-            .chars();
-        let input_string = &String::from_iter(input_text);
-        let multiplier = crop_letters(input_string, 1).parse::<i32>().unwrap();
+        let input_string = &String::from_iter(dir.chars());
+        let next_turn = input_string.chars().next().unwrap().to_string();
+        let num_blocks = crop_letters(input_string, 1).parse::<i32>().unwrap();
+        let is_pos_x = axis == 0 && direction == 1;
+        let is_neg_x = axis == 0 && direction == -1;
+        let is_pos_y = axis == 1 && direction == 1;
+        let is_neg_y = axis == 1 && direction == -1;
 
-        if is_x && is_positive {
-            coord_to_mod = 1;
-            if direction == "R" {
-                current_mult = -1;
+        if is_pos_x {
+            axis = 1;
+            if next_turn == "R" {
+                direction = -1;
             } else {
-                current_mult = 1;
+                direction = 1;
             }
-        } else if is_x && is_negative {
-            coord_to_mod = 1;
-            if direction == "R" {
-                current_mult = 1;
+        } else if is_neg_x {
+            axis = 1;
+            if next_turn == "R" {
+                direction = 1;
             } else {
-                current_mult = -1;
+                direction = -1;
             }
-        } else if is_y && is_positive {
-            coord_to_mod = 0;
-            if direction == "R" {
-                current_mult = 1;
+        } else if is_pos_y {
+            axis = 0;
+            if next_turn == "R" {
+                direction = 1;
             } else {
-                current_mult = -1;
+                direction = -1;
             }
-        } else if is_y && is_negative {
-            coord_to_mod = 0;
-            if direction == "R" {
-                current_mult = -1;
+        } else if is_neg_y {
+            axis = 0;
+            if next_turn == "R" {
+                direction = -1;
             } else {
-                current_mult = 1;
+                direction = 1;
             }
         }
-        current_loc = update_coordinate((coord_to_mod, multiplier, current_mult, current_loc));
+        current_coordinates = update_coordinate((axis, num_blocks, direction, current_coordinates));
     }
-
-    println!("{:?}", current_loc);
-    println!("{:?}", current_loc[0].abs() + current_loc[1].abs())
+    println!("{:?}", current_coordinates);
+    println!("{:?}", current_coordinates[0].abs() + current_coordinates[1].abs());
 }
 
 fn update_coordinate(
-    (coord_to_mod, input_mult, current_mult, mut current_loc): (i32, i32, i32, [i32; 2]),
+    (axis, input_mult, direction, mut current_coordinates): (i32, i32, i32, [i32; 2]),
 ) -> [i32; 2] {
-    current_loc[coord_to_mod as usize] += input_mult * current_mult;
-    return current_loc;
+    current_coordinates[axis as usize] += input_mult * direction;
+    return current_coordinates;
 }
 
 fn crop_letters(s: &str, pos: usize) -> &str {
@@ -64,4 +63,9 @@ fn crop_letters(s: &str, pos: usize) -> &str {
         Some((pos, _)) => &s[pos..],
         None => "",
     }
+}
+
+fn read_file_string(filepath: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let data = fs::read_to_string(filepath)?;
+    Ok(data)
 }

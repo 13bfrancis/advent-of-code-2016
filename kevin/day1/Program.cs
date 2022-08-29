da8c95f9fb183d4fca0d74bc5522183323677c74
Kevin Innes <kinnes@cloud-cme.com>
2022-08-29 07:21:11 -0400

    cleaning up check for second visit

--- kevin/day1/Program.cs
+++ kevin/day1/Program.cs
@@ -1,5 +1,4 @@
-﻿
-List<string> input = "L3, R2, L5, R1, L1, L2, L2, R1, R5, R1, L1, L2, R2, R4, L4, L3, L3, R5, L1, R3, L5, L2, R4, L5, R4, R2, L2, L1, R1, L3, L3, R2, R1, L4, L1, L1, R4, R5, R1, L2, L1, R188, R4, L3, R54, L4, R4, R74, R2, L4, R185, R1, R3, R5, L2, L3, R1, L1, L3, R3, R2, L3, L4, R1, L3, L5, L2, R2, L1, R2, R1, L4, R5, R4, L5, L5, L4, R5, R4, L5, L3, R4, R1, L5, L4, L3, R5, L5, L2, L4, R4, R4, R2, L1, L3, L2, R5, R4, L5, R1, R2, R5, L2, R4, R5, L2, L3, R3, L4, R3, L2, R1, R4, L5, R1, L5, L3, R4, L2, L2, L5, L5, R5, R2, L5, R1, L3, L2, L2, R3, L3, L4, R2, R3, L1, R2, L5, L3, R4, L4, R4, R3, L3, R1, L3, R5, L5, R1, R5, R3, L1".Split(",").ToList();
+﻿List<string> input = "L3, R2, L5, R1, L1, L2, L2, R1, R5, R1, L1, L2, R2, R4, L4, L3, L3, R5, L1, R3, L5, L2, R4, L5, R4, R2, L2, L1, R1, L3, L3, R2, R1, L4, L1, L1, R4, R5, R1, L2, L1, R188, R4, L3, R54, L4, R4, R74, R2, L4, R185, R1, R3, R5, L2, L3, R1, L1, L3, R3, R2, L3, L4, R1, L3, L5, L2, R2, L1, R2, R1, L4, R5, R4, L5, L5, L4, R5, R4, L5, L3, R4, R1, L5, L4, L3, R5, L5, L2, L4, R4, R4, R2, L1, L3, L2, R5, R4, L5, R1, R2, R5, L2, R4, R5, L2, L3, R3, L4, R3, L2, R1, R4, L5, R1, L5, L3, R4, L2, L2, L5, L5, R5, R2, L5, R1, L3, L2, L2, R3, L3, L4, R2, R3, L1, R2, L5, L3, R4, L4, R4, R3, L3, R1, L3, R5, L5, R1, R5, R3, L1".Split(",").ToList();
 //List<string> input = "R8, R4, R4, R8".Split(",").ToList();
 
 int x = 0;
@@ -36,22 +35,16 @@ foreach (var item in input)
                 x--;
                 break;
         }
-        var selectLocation = from c in locationList where c.xcord == x && c.ycord == y select c;
-        bool visitTwice = false;
 
-        if (selectLocation.Any() && !locationList.Where(c => c.VisitedTwice).Any())
-        {
-            visitTwice = true;
-        }
-
-        locationList.Add(new Location { xcord = x, ycord = y, CurrentDirection = CurrentDirection, VisitedTwice = visitTwice });
+        locationList.Add(new Location { xcord = x, ycord = y, CurrentDirection = CurrentDirection });
     }
 }
 
-var selectSecondVisit = (from c in locationList where c.VisitedTwice == true select c).FirstOrDefault();
+var selectSecondVisit = locationList.GroupBy(c => new { c.xcord, c.ycord }).Select(g => new { Xcord = g.Key.xcord, yCord = g.Key.ycord, count = g.Count() }).Where(c => c.count > 1).FirstOrDefault();
+
 
 var blocksAway = Math.Abs(x) + Math.Abs(y);
-var secondLocation = Math.Abs(selectSecondVisit.xcord) + Math.Abs(selectSecondVisit.ycord);
+var secondLocation = Math.Abs(selectSecondVisit.Xcord) + Math.Abs(selectSecondVisit.yCord);
 
 Console.Write($"Blocks Away: {blocksAway}");
 Console.Write($"Blocks Away first location to see twice: {secondLocation}");
--- kevin/day1/Rotation.cs
+++ kevin/day1/Rotation.cs
@@ -3,5 +3,4 @@ public class Location
     public int xcord { get; set; }
     public int ycord { get; set; }
     public Direction CurrentDirection { get; set; }
-    public bool VisitedTwice { get; set; }
 }

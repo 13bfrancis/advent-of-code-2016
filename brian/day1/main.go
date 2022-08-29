ce4e1d2c996dd32f3d82e851dc4044dabd7f4227
Brian Francis <brianfrancis@Brians-MacBook-Pro.local>
2022-08-28 22:14:41 -0400

    fix file parsing

--- brian/day1/main.go
+++ brian/day1/main.go
@@ -2,79 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type location struct {
-	x                int
-	y                int
-	currentDirection string
-}
-
 func main() {
-	playerLoc := location{
-		x:                0,
-		y:                0,
-		currentDirection: "north",
+	moves := fileToCode("inputs.txt")
+
+	for _, move := range moves {
+		direction, numOfMoves := seperateDirectionAndNumber(move)
+
+		fmt.Println(direction, numOfMoves)
 	}
-
-	inputs := readFile("inputs.txt")
-
-	for _, move := range inputs {
-		playerLoc.changeDirection(move)
-	}
-
-	fmt.Println(getDistance(playerLoc))
 }
 
-func (loc *location) changeDirection(move string) {
-	moveDirection, moveAmount := string(move[1]), string(move[2:])
-	convMoveAmount, _ := strconv.Atoi(moveAmount)
-	fmt.Println("direction", moveDirection, len(moveDirection))
-
-	if loc.currentDirection == "north" {
-		if moveDirection == "R" {
-			loc.currentDirection = "east"
-			loc.x += convMoveAmount
-		} else {
-			loc.currentDirection = "west"
-			loc.x -= convMoveAmount
-		}
-	} else if loc.currentDirection == "south" {
-		if moveDirection == "R" {
-			loc.currentDirection = "west"
-			loc.x -= convMoveAmount
-		} else {
-			loc.currentDirection = "east"
-			loc.x += convMoveAmount
-		}
-	} else if loc.currentDirection == "east" {
-		if moveDirection == "R" {
-			loc.currentDirection = "south"
-			loc.y -= convMoveAmount
-		} else {
-			loc.currentDirection = "north"
-			loc.y += convMoveAmount
-		}
-	} else if loc.currentDirection == "west" {
-		if moveDirection == "R" {
-			loc.currentDirection = "north"
-			loc.y += convMoveAmount
-		} else {
-			loc.currentDirection = "south"
-			loc.y -= convMoveAmount
-		}
-	}
-	fmt.Println(*loc)
-}
-
-func getDistance(loc location) int {
-	return (int(math.Abs(float64(loc.x)))) + int(math.Abs(float64(loc.y)))
-}
-func readFile(fileName string) []string {
+func fileToCode(fileName string) []string {
 	bs, _ := os.ReadFile(fileName)
-	return strings.Split(string(bs), ",")
+
+	return strings.Split(string(bs), ", ")
+}
+
+func seperateDirectionAndNumber(dirAndNum string) (string, int) {
+	dir, numMoves := string(dirAndNum[0]), dirAndNum[1:]
+	convertedNumMoves, _ := strconv.Atoi(numMoves)
+
+	return dir, convertedNumMoves
 }

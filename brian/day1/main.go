86afa6d3fd418816b9e5a410d0b51484dc08b889
Brian Francis <brianfrancis@Brians-MacBook-Pro.local>
2022-08-28 23:44:13 -0400

    merged refactor-go branch into main

--- brian/day1/main.go
+++ brian/day1/main.go
@@ -2,79 +2,101 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type location struct {
-	x                int
-	y                int
-	currentDirection string
+type gps struct {
+	direction string
+	x         int
+	y         int
+	meta      moveMeta
+}
+
+type moveMeta struct {
+	dirLookup int
+}
+
+var directionMap = map[int]string{
+	0: "north",
+	1: "east",
+	2: "south",
+	3: "west",
 }
 
 func main() {
-	playerLoc := location{
-		x:                0,
-		y:                0,
-		currentDirection: "north",
+	moves := fileToCode("inputs.txt")
+	gps := gps{
+		direction: "north",
+		x:         0,
+		y:         0,
+		meta: moveMeta{
+			dirLookup: 0,
+		},
 	}
 
-	inputs := readFile("inputs.txt")
+	for _, move := range moves {
+		direction, numOfMoves := seperateDirectionAndNumber(move)
 
-	for _, move := range inputs {
-		playerLoc.changeDirection(move)
+		gps.setDirBasedOnMove(direction)
+		gps.moveNTimes(numOfMoves)
 	}
 
-	fmt.Println(getDistance(playerLoc))
+	fmt.Println(gps.calculateDistance())
 }
 
-func (loc *location) changeDirection(move string) {
-	moveDirection, moveAmount := string(move[1]), string(move[2:])
-	convMoveAmount, _ := strconv.Atoi(moveAmount)
-	fmt.Println("direction", moveDirection, len(moveDirection))
-
-	if loc.currentDirection == "north" {
-		if moveDirection == "R" {
-			loc.currentDirection = "east"
-			loc.x += convMoveAmount
-		} else {
-			loc.currentDirection = "west"
-			loc.x -= convMoveAmount
-		}
-	} else if loc.currentDirection == "south" {
-		if moveDirection == "R" {
-			loc.currentDirection = "west"
-			loc.x -= convMoveAmount
-		} else {
-			loc.currentDirection = "east"
-			loc.x += convMoveAmount
-		}
-	} else if loc.currentDirection == "east" {
-		if moveDirection == "R" {
-			loc.currentDirection = "south"
-			loc.y -= convMoveAmount
-		} else {
-			loc.currentDirection = "north"
-			loc.y += convMoveAmount
-		}
-	} else if loc.currentDirection == "west" {
-		if moveDirection == "R" {
-			loc.currentDirection = "north"
-			loc.y += convMoveAmount
-		} else {
-			loc.currentDirection = "south"
-			loc.y -= convMoveAmount
-		}
-	}
-	fmt.Println(*loc)
-}
-
-func getDistance(loc location) int {
-	return (int(math.Abs(float64(loc.x)))) + int(math.Abs(float64(loc.y)))
-}
-func readFile(fileName string) []string {
+func fileToCode(fileName string) []string {
 	bs, _ := os.ReadFile(fileName)
-	return strings.Split(string(bs), ",")
+
+	return strings.Split(string(bs), ", ")
+}
+
+func seperateDirectionAndNumber(dirAndNum string) (string, int) {
+	dir, numMoves := string(dirAndNum[0]), dirAndNum[1:]
+	convertedNumMoves, _ := strconv.Atoi(numMoves)
+
+	return dir, convertedNumMoves
+}
+
+func (g *gps) setDirBasedOnMove(moveDir string) {
+	if moveDir == "L" {
+		g.meta.dirLookup--
+	} else if moveDir == "R" {
+		g.meta.dirLookup++
+	}
+
+	if g.meta.dirLookup == -1 {
+		g.meta.dirLookup = 3
+	}
+	if g.meta.dirLookup == 4 {
+		g.meta.dirLookup = 0
+	}
+
+	g.direction = directionMap[g.meta.dirLookup]
+}
+
+func (g *gps) moveNTimes(n int) {
+	if g.direction == "north" {
+		g.y += n
+	} else if g.direction == "south" {
+		g.y -= n
+	} else if g.direction == "east" {
+		g.x += n
+	} else if g.direction == "west" {
+		g.x -= n
+	}
+}
+
+func (g *gps) calculateDistance() int {
+	return Abs(g.x) + Abs(g.y)
+}
+
+// absolute value function
+func Abs(num int) int {
+	if num < 0 {
+		return -num
+	}
+
+	return num
 }

c6bc47adab83ee8466471a54da9ce76e5bb9637a
KMunk <kmunk@cloud-cme.com>
2022-09-04 22:41:54 -0400

    Day 2 Part 2 completion

--- kory/day2/day2.js
+++ kory/day2/day2.js
@@ -6,33 +6,51 @@ const keypad = [
     [7, 8, 9]
 ];
 
+const newKeyPad = [
+    [null, null, '1', null, null],
+    [null, '2', '3', '4', null],
+    ['5', '6', '7', '8', '9'],
+    [null, 'A', 'B', 'C', null],
+    [null, null, 'D', null, null]
+]
 readFile('input.txt', "utf8", (_err, data) => {
     let instructions = data.split(/\n/).map(row => row.split(''));
 
-    let xStart = 1;
-    let yStart = 1;
-    let bathroomCode = '';
+    let pos1 = {x: 1, y: 1};
+    let pos2 = {x: 0, y: 2};
     
+    let bathroomCode = '';
+    let newBathroomCode = '';
+
     instructions.forEach(row => {
         row.forEach(op => {
             switch (op.toUpperCase()) {
                 case 'U':
-                    yStart = Math.max(0, yStart - 1);
+                    pos1.y = Math.max(0, --pos1.y);
+                    pos2.y = Math.max(0, --pos2.y);
+                    pos2.y = newKeyPad[pos2.y][pos2.x] ? pos2.y : ++pos2.y;
                     break;
                 case 'D':
-                    yStart = Math.min(yStart + 1, 2);
+                    pos1.y = Math.min(++pos1.y, 2);
+                    pos2.y = Math.min(++pos2.y, 4);
+                    pos2.y = newKeyPad[pos2.y][pos2.x] ? pos2.y : --pos2.y;
                     break;
                 case 'L':
-                    xStart = Math.max(0, xStart -1);
+                    pos1.x = Math.max(0, --pos1.x);
+                    pos2.x = Math.max(0, --pos2.x);
+                    pos2.x = newKeyPad[pos2.y][pos2.x] ? pos2.x : ++pos2.x;
                     break;
                 case 'R':
-                    xStart = Math.min(xStart + 1, 2);
+                    pos1.x = Math.min(++pos1.x, 2);
+                    pos2.x = Math.min(++pos2.x, 4);
+                    pos2.x = newKeyPad[pos2.y][pos2.x] ? pos2.x : --pos2.x;
                     break;
             }
         });
-        
-        bathroomCode += keypad[yStart][xStart];
+
+        bathroomCode += keypad[pos1.y][pos1.x];
+        newBathroomCode += newKeyPad[pos2.y][pos2.x];
     });
 
-    console.table({bathroomCode});
+    console.table({ bathroomCode, newBathroomCode });
 });

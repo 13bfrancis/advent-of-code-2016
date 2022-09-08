71dc13643f785292e72fb737046ee3d5e7c3405b
bdriggs <65035792+bdriggs@users.noreply.github.com>
2022-09-07 21:00:33 -0700

    Updating my solutions

--- britton/day2/day2.rs
+++ britton/day2/day2.rs
@@ -6,6 +6,13 @@ fn main() {
     let mut x: usize = 0;
     let mut y: usize = 2;
     let mut code: Vec<char> = Vec::new();
+    // let num_pad: Vec<[char; 5]> = vec![
+    //     ['-', '-', '-', '-', '-'],
+    //     ['-', '1', '2', '3', '-'],
+    //     ['-', '4', '5', '6', '-'],
+    //     ['-', '7', '8', '9', '-'],
+    //     ['-', '-', '-', '-', '-'],
+    // ];
     let num_pad: Vec<[char; 5]> = vec![
         ['-', '-', '1', '-', '-'],
         ['-', '2', '3', '4', '-'],
@@ -13,6 +20,7 @@ fn main() {
         ['-', 'A', 'B', 'C', '-'],
         ['-', '-', 'D', '-', '-'],
     ];
+
     let mut position: char = num_pad[y][x];
 
     directions.for_each(|line: &str| {

9707245c2a42907b7d03d2ec5c715e840140769d
bdriggs <65035792+bdriggs@users.noreply.github.com>
2022-09-07 20:51:34 -0700

    Update day2part2.rs

--- britton/day2/day2part2.rs
+++ britton/day2/day2part2.rs
@@ -3,59 +3,51 @@ use std::fs;
 fn main() {
     let input: String = read_file_string("input.txt").unwrap();
     let directions: std::str::Split<&str> = input.split("\n");
-    let num_pad: Vec<[&str; 5]> = vec![
-        ["-", "-", "1", "-", "-"],
-        ["-", "2", "3", "4", "-"],
-        ["5", "6", "7", "8", "9"],
-        ["-", "A", "B", "C", "-"],
-        ["-", "-", "D", "-", "-"],
+    let num_pad: Vec<[char; 5]> = vec![
+        ['-', '-', '1', '-', '-'],
+        ['-', '2', '3', '4', '-'],
+        ['5', '6', '7', '8', '9'],
+        ['-', 'A', 'B', 'C', '-'],
+        ['-', '-', 'D', '-', '-'],
     ];
 
     let mut x: usize = 0;
     let mut y: usize = 2;
-    let mut code: Vec<&str> = Vec::new();
-    let mut position: &str = num_pad[y][x];
+    let mut code: Vec<char> = Vec::new();
+    let mut position: char = num_pad[y][x];
 
     directions.for_each(|line: &str| {
         line.chars().for_each(|c: char| {
-            let mut is_button: bool = false;
+            let is_button: bool;
 
-            if c == 'L' {
-                let mut new_x: usize = x;
-                if x > 0 {
-                    new_x = x - 1;
-                    is_button = num_pad[y][new_x] != "-";
-                }
+            if c == 'L' && x > 0 {
+                let new_x: usize = x - 1;
+                is_button = num_pad[y][new_x] != '-';
+
                 if is_button {
                     position = num_pad[y][new_x];
                     x = new_x;
                 }
-            } else if c == 'R' {
+            } else if c == 'R' && x <= 3 {
                 let new_x: usize = x + 1;
+                is_button = num_pad[y][new_x] != '-';
 
-                if new_x <= 4 {
-                    is_button = num_pad[y][new_x] != "-";
-                }
                 if is_button {
                     position = num_pad[y][new_x];
                     x = new_x;
                 }
-            } else if c == 'U' {
-                let mut new_y: usize = y;
-                if y > 0 {
-                    new_y = y - 1;
-                    is_button = num_pad[new_y][x] != "-";
-                }
+            } else if c == 'U' && y > 0 {
+                let new_y: usize = y - 1;
+                is_button = num_pad[new_y][x] != '-';
+
                 if is_button {
                     position = num_pad[new_y][x];
                     y = new_y;
                 }
-            } else if c == 'D' {
+            } else if c == 'D' && y <= 3 {
                 let new_y: usize = y + 1;
+                is_button = num_pad[new_y][x] != '-';
 
-                if new_y <= 4 {
-                    is_button = num_pad[new_y][x] != "-";
-                }
                 if is_button {
                     position = num_pad[new_y][x];
                     y = new_y;
